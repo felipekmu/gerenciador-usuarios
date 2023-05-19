2ff43fac8f73b8dad2697d98b1b0be5b38b6769a
felipekmu <132099333+felipekmu@users.noreply.github.com>
2023-05-19 17:21:25 -0300

    Aula do Dia 19 de Maio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import Listagem from "./Componentes/Listagem"
 function App() {
+  const Usuario = {
+    //chave: valor
+    nome: "Felipe",
+    idade: 19,
+    status: true,
+    endereco: {
+      rua: "Rua X",
+      numero: 139,
+      complemento: "ap 20"
+    },
+    vacinas_covid19: ["Coronavac", "Coronavac", "Pfizer", "J&J", "Outra"]
+  }
+  
+  const usuarios = [
+      {nome: "Felipe", status: true},
+      {nome: "Conrado", status: true},
+      {nome: "Maria", status: false},
+      {nome: "Pedrita", status: false}
+  ] 
+console.log(usuarios)
+
+
+  //console.log(Usuario)
+  //console.log(Usuario.endereco.rua, Usuario.endereco.complemento)
+  //console.log(Object.keys(Usuario))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="container">
+    <div className="d-flex justify-content-around">
+    <Listagem usuarios={usuarios}/>
+    
+     <div className="caixa"> 
+      <h2> Cadastro </h2>
     </div>
+    
+    </div>
+</div>
   );
 }
 
---
+++ src/Componentes/Listagem.js
@@ -0,0 +1,16 @@
+function Listagem(props){
+    return(
+    <div className="caixa">
+      <h2> Listagem </h2>
+      {
+        props.usuarios.map( item => {
+          return <div style={{border:"1px solid #CCC"}}>
+           <p> Nome: {item.nome} </p>
+           <p> Status: {item.status == true ? "Ativo" : "Banido"} </p>
+           </div>
+        })
+      }
+    </div> 
+    )
+}
+export default Listagem;
